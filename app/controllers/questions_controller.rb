aeb113b34baa40265265a5785a94bb0d52134f87
Adrian Simionescu <simionescuadrian@yahoo.com>
2022-02-14 14:54:01 +0400

    Solution

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+    @question = params[:question]
+  end
+
+  def answer
+    @question = params[:question]
+    if @question[-1, 1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question != 'I am going to work'
+      @answer = "I don't care, get dressed and go to work!"
+    else
+      @answer = 'Great!'
+    end
+  end
+end
